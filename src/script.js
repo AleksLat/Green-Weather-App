2bc8b45b20a8cc79aaf820aab4f8bf719d30df42
AleksLat <66917008+AleksLat@users.noreply.github.com>
2020-08-08 17:21:36 +0200

    Updated the time

--- src/script.js
+++ src/script.js
@@ -1,3 +1,30 @@
+function formatDate(timestamp){
+
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours<10){
+     hours = `0${hours}`;
+   }
+    let minutes = date.getMinutes();
+       if (minutes<10){
+         minutes = `0${minutes}`;
+       }
+    let days = [
+           "Sun", 
+           "Mon", 
+           "Tue", 
+           "Wed",
+           "Thu",
+           "Fri",
+           "Sat"
+       ];
+    let day = days[date.getDay()];
+   
+   return `${day} ${hours}:${minutes}`;
+   }
+   
+   
+
 function showTemperature(response){
 
     let tempElement = document.querySelector("#currTemperature");
@@ -6,7 +33,7 @@ function showTemperature(response){
     let humidElement = document.querySelector("#humidity");
     let pressureElement = document.querySelector("#pressure");
     let windElement = document.querySelector("#wind-speed");
-    
+    let dateElement= document.querySelector("#date");
    
     tempElement.innerHTML=Math.round(response.data.main.temp);
     cityElement.innerHTML=response.data.name;
@@ -14,6 +41,7 @@ function showTemperature(response){
     humidElement.innerHTML=response.data.main.humidity;
     pressureElement.innerHTML=response.data.main.pressure;
     windElement.innerHTML=Math.round(response.data.wind.speed);
+    dateElement.innerHTML=formatDate(response.data.dt*1000);
    
    }
 
