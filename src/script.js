791ec6455093514cf196cef46e34f003b581c157
AleksLat <66917008+AleksLat@users.noreply.github.com>
2020-08-10 14:28:59 +0200

    Added Forecast for the 5 days

--- src/script.js
+++ src/script.js
@@ -1,14 +1,6 @@
 function formatDate(timestamp){
 
     let date = new Date(timestamp);
-    let hours = date.getHours();
-    if (hours<10){
-     hours = `0${hours}`;
-   }
-    let minutes = date.getMinutes();
-       if (minutes<10){
-         minutes = `0${minutes}`;
-       }
     let days = [
            "Sun", 
            "Mon", 
@@ -20,10 +12,22 @@ function formatDate(timestamp){
        ];
     let day = days[date.getDay()];
    
-   return `${day} ${hours}:${minutes}`;
+   return `${day} ${formatHours(timestamp)}`;
    }
    
-   
+function formatHours(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours<10){
+     hours = `0${hours}`;
+   }
+    let minutes = date.getMinutes();
+       if (minutes<10){
+         minutes = `0${minutes}`;
+       }
+  
+    return `${hours}:${minutes}`;
+  }
 
 function showTemperature(response){
 
@@ -51,13 +55,57 @@ function showTemperature(response){
             response.data.weather[0].description);
    }
 
-   function search(city) {
+function displayForecast(response){
+  
+    let forecastElement = document.querySelector("#futureForecast");
+    let forecast = null;
+   
+    forecastElement.innerHTML=null;
+   
+    for (let index = 0; index < 5; index++) {
+     forecast = response.data.list[index];
+     forecastElement.innerHTML+=`
+       <div class="col-xs-1 firstOffset day" id="p1">
+       <h3 class="hourForecast">
+          ${formatHours((forecast.dt)*1000)}
+       </h3>
+        <img 
+        src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" 
+        class="smallImage"
+        alt="weathericon">
+       <div class="temperatureMax">
+           <strong>
+                ${Math.round(forecast.main.temp_max)}C
+            </strong>
+       </div>
+       <div class="temperatureMin">
+             ${Math.round(forecast.main.temp_min)}C
+       </div>
+     </div>
+       `;
+      }
+    }
+
+function search(city) {
     let apiKey="577759180b250273cb6dd606dacb4cd6";
     let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showTemperature)
+
+    apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
   
   }
 
+function searchLocation(position){
+    let apiKey = "577759180b250273cb6dd606dacb4cd6";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude
+    }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemperature);
+  
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude
+  }&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayForecast);
+}
 
 function handleSubmit(event){
     event.preventDefault();
@@ -74,13 +122,18 @@ function handleSubmit(event){
     let tempElement=document.querySelector("#currTemperature");
     tempElement.innerHTML=Math.round(fahrenheitTemp);
   }
-  
+
   function showcelsiusTemp(event) {
     event.preventDefault(); 
     celsiusLink.classList.add("active");
     fahrenheitLink.classList.remove("active");
     let tempElement=document.querySelector("#currTemperature");
     tempElement.innerHTML = Math.round(celsiusTemp);
+  }
+
+function showCurrLoc(event) {
+    event.preventDefault();
+    navigator.geolocation.getCurrentPosition(searchLocation);
   }
 
 let celsiusTemp = null;
@@ -93,6 +146,9 @@ fahrenheitLink.addEventListener("click", showfahrenheitTemp);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showcelsiusTemp );
+
+let currentLocation = document.querySelector("#local-submit-info");
+currentLocation.addEventListener("click", showCurrLoc);
 
 search("San Francisco");
  
